16aaa3284f80ab59f294164962b68e54b402cba4
john <138552942+johnjimenez-it@users.noreply.github.com>
2024-03-25 20:36:28 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// In script.js
+function searchFriends() {
+    console.log('Searching for friends...');
+    // Add your search functionality here
+}
+
+function sendFriendRequest() {
+    console.log('Sending friend request...');
+    // Add your send request functionality here
+}
